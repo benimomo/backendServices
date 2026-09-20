07914129ae6f849dcc1086221570bc7cd1c36e3f
nabeni <eakrani@proton.me>
2026-09-20 17:12:25 +0330

    fix the apigatewayService

--- src/AuthService/Application/DTOs/RegisterDto.cs
+++ src/AuthService/Application/DTOs/RegisterDto.cs
@@ -1,9 +1,13 @@
-﻿namespace AuthService.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+public class RegisterDto
 {
-    public class RegisterDto
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-        public required string Email { get; set; }
-    }
+    public required string Username { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "password should be between 8 to 100 characters.")]
+    public required string Password { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
 }
